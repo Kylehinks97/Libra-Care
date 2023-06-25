8be2f910a52bb50d01161c61c26cabc352d45fc9
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-06-25 14:51:02 +0100

    project set up and made a start on nav bar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from "./Home.tsx"
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav.tsx"
+
+export default function Home() {
+
+
+
+    return (
+        <>
+        <Nav />
+        <h1>Libra Care</h1>
+        </>
+    )
+}
