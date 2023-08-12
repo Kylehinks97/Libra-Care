e4e8c8ca66fbf5ea867157af0f763360223dbf8e
Kylehinks97 <kyle.s.hinks@outlook.com>
2023-08-12 09:51:49 +0100

    made contact us page link to phone calls and emails

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,9 +1,13 @@
 import Footer from "./Footer";
 import FormComponent from "./FormComponent";
 import FinalFooter from "./FinalFooter.jsx";
-import "./styles/ContactUs.css"
+import "./styles/ContactUs.css";
 
 export default function ContactUs() {
+  function openEmailWindow() {
+    return window.open("mailto:libracare@outlook.com");
+  }
+
   return (
     <>
       <h1
@@ -18,7 +22,7 @@ export default function ContactUs() {
       <div
         style={{
           marginTop: "2em",
-          marginBottom: "2em"
+          marginBottom: "2em",
         }}
         id="contact-us-wrapper"
       >
@@ -26,28 +30,39 @@ export default function ContactUs() {
           style={{
             marginTop: "1em",
           }}
+          onClick={openEmailWindow}
         >
           Email
         </h4>
-        <p>libracare@outlook.com</p>
-        <h4>Telephone</h4>
-        <p>01922 234 453</p>
-        <h4>Mobile</h4>
-        <p>07974 733 121</p>
+        <p onClick={openEmailWindow}>libracare@outlook.com</p>
+        <a
+          href="tel:01922234453"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <h4>Telephone</h4>
+          <p>01922 234 453</p>
+        </a>
+        <a
+          href="tel:07974733121"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <h4>Mobile</h4>
+          <p>07974 733 121</p>
+        </a>
         <h4>Address</h4>
         <p style={{ marginBottom: "0px" }}>Suite 58 Beacon Buildings</p>
         <p style={{ marginBottom: "0px" }}>Leighswood Road</p>
         <p style={{ marginBottom: "0px" }}>Aldridge</p>
         <p>WS9 8AA</p>
       </div>
-        <p
-          className="d-flex justify-content-center"
-          style={{
-            marginTop: "2em",
-          }}
-        >
-          or alternatively...
-        </p>
+      <p
+        className="d-flex justify-content-center"
+        style={{
+          marginTop: "2em",
+        }}
+      >
+        or alternatively...
+      </p>
       <div
         className="d-flex justify-content-center"
         style={{ marginBottom: "2em" }}
